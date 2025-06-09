d5f29337b55165d2ee4ba1fec06b6bd37e944da1
Shivam <shivamchawla678@gmail.com>
2025-06-09 12:26:39 +0530

    Deleted institution routes

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/institution", institutionRoutes); // institution routes
 
 app.get("/", (req, res) => {
   res.send("API is running...");
--- routes/institutionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerInstitution, loginInstitution } from "../controllers/institutionController.js";
-
-const router = express.Router();
-
-router.post("/register-institution", registerInstitution);
-router.post("/login-institution", loginInstitution);
-
-export default router;
