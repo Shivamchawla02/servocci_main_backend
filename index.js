78d9229f7996342cffaa2d3d6854e7c0afdbff81
Shivam <shivamchawla678@gmail.com>
2025-09-04 17:13:20 +0530

    Added cors issue fallback

--- index.js
+++ index.js
@@ -15,10 +15,26 @@ import blogRoutes from "./routes/blogRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: ["https://servocci.com", "https://placements.servocci.com"],
-  credentials: true,
-}));
+const allowedOrigins = [
+  "https://servocci.com",
+  "https://placements.servocci.com"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
 
